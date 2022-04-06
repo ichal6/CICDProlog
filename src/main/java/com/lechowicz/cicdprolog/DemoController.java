90427077424660cac64c0cdaf00ff8a541c2afb4
Michael Lechowicz <ichal.lechowicz@gmail.com>
2022-04-06 14:22:58 +0200

    Commit test for CI/CD

--- src/main/java/com/lechowicz/cicdprolog/DemoController.java
+++ src/main/java/com/lechowicz/cicdprolog/DemoController.java
@@ -9,6 +9,6 @@ public class DemoController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() {
-        return "Hello world from Spring Boot";
+        return "Hello world from Spring Boot.\nTest CI/CD";
     }
 }
