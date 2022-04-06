3b71ca59c4a671ff0b3bef196b94bb805ece0d6b
Michael Lechowicz <ichal.lechowicz@gmail.com>
2022-04-06 17:40:55 +0200

    Add simple test

---
+++ src/test/java/com/lechowicz/cicdprolog/DemoControllerTests.java
@@ -0,0 +1,12 @@
+package com.lechowicz.cicdprolog;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DemoControllerTests {
+    @Test
+    void test()
+    {
+        Assertions.assertEquals(4,4);
+    }
+}
